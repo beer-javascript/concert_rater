d042cffefb91f2b9baf275085276be0172d8e5b3
Ryan Loos <RLoos289@gmail.com>
2016-11-08 00:05:26 -0800

    add firebase, bootstrap, and sass dependencies, verify that database info can appear on the front page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+    concerts: this.store.findAll('concert'),
+    // ratings: this.store.findAll('rating'),
+    });
+  }
 });
